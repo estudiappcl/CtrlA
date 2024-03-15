8f45e2a655768e49e5cb6e8b840108a0c407da80
Pinkq1 <diegomedina99j@gmail.com>
2024-03-15 19:31:51 -0300

    recover

--- src/app/modules/recover-password/recover-password.component.ts
+++ src/app/modules/recover-password/recover-password.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-recover-password',
   templateUrl: './recover-password.component.html',
-  styleUrl: './recover-password.component.css'
+  styleUrls: ['./recover-password.component.css']
 })
 export class RecoverPasswordComponent {
+  password: string = '';
+  showPassword: boolean = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
