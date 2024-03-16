2486dc3d6a5da436001101a48f79808934ac342d
Pinkq1 <diegomedina99j@gmail.com>
2024-03-16 19:17:34 -0300

    modal dinamico listo

--- src/app/modules/Custom/modal-consulta-individual/modal-consulta-individual.component.ts
+++ src/app/modules/Custom/modal-consulta-individual/modal-consulta-individual.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-modal-consulta-individual',
   templateUrl: './modal-consulta-individual.component.html',
@@ -8,8 +8,20 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
 export class ModalConsultaIndividualComponent {
   @Input() tipoConsulta: 'usuario' | 'agencia' | 'inventario' | null = null;
   @Output() cerrar = new EventEmitter<void>();
-
+  constructor(private router: Router) {} 
   cerrarModal(): void {
     this.cerrar.emit();
+  }
+  buscar(): void {
+    if (this.tipoConsulta === 'usuario') {
+      // Navega a la ruta para consulta de usuario/rut
+      this.router.navigate(['/data-usuario-rut']);
+    } else if (this.tipoConsulta === 'agencia') {
+      // Navega a la ruta para consulta de agencia/DPC
+      this.router.navigate(['/agencia-dcp']);
+    } else if (this.tipoConsulta === 'inventario') {
+      // Navega a la ruta para consulta de número de inventario
+      this.router.navigate(['/numero-inventario']);
+    }
   }
 }
