fc10b7bc7ac243d773ec799f7ce5c05c8829bfd4
Pinkq1 <diegomedina99j@gmail.com>
2024-03-20 20:31:25 -0300

    modal editar confirmar

--- src/app/modules/Custom/modal-editar/modal-editar.component.ts
+++ src/app/modules/Custom/modal-editar/modal-editar.component.ts
@@ -6,9 +6,14 @@ import { Component,Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './modal-editar.component.css'
 })
 export class ModalEditarComponent {
-  @Input() mensajeModalEditar: string = '';
   @Output() cerrar = new EventEmitter<void>();
+  @Output() exitoEditar = new EventEmitter<void>(); 
+  @Input() mensajeModalEditar: string = '';
+
   cerrarModal(): void {
     this.cerrar.emit(); // Emite el evento para ser escuchado por el componente padre
+  }
+  editarUsuarioExitoso(): void {
+    this.exitoEditar.emit(); // Emite el evento de edición exitosa
   }
 }
